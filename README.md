db13a7ad83fef0872e8d1b5f47c2b0ae61210ea3
John Weland <jweland@icsolutions.com>
2020-07-01 15:34:13 -0500

    code cleanup and commenting

---
+++ README.md
@@ -0,0 +1,5 @@
+# damnedcatstudio.github.io
+The website for Damned Cat Studio
+
+### Note
+pushing to master runs CI to clean up code.
--- js/main.js
+++ js/main.js
@@ -1,23 +1,60 @@
+/**
+ * @fileoverview Contains the namespace {@link application}
+ *
+ * @copyright © Damned Cat Studio - All rights reserved
+ */
+
+"use strict";
+
+/**
+ * Javascript for main application/site
+ *
+ * @namespace
+ */
 const application = {
-    windowLoad: () => {
-        feather.replace();
+  /**
+   * Fire off when DOM becomes ready
+   */
+  windowLoad: () => {
+    feather.replace(); // from feather-icons
+    let copy = document.getElementById("copyDate");
+    if (copy !== null) {
+      application.setCopyright(copy);
     }
-}
+  },
+  /**
+   * Update the year to the current year if it is not
+   * @param {HTMLElement} el
+   * 
+   * @return {void}
+   */
+  setCopyright: (el) => {
+    let dttm = new Date().getFullYear();
+    if (el.innerHTML !== dttm.toString()) {
+      el.innerHTML = dttm.toString();
+    }
+  },
+};
 
+/**
+ * Check the state of the DOM & attach windowLoad
+ */
 if (
-    document.readyState === "complete" ||
-    (document.readyState !== "loading" && !document.documentElement.doScroll)
+  document.readyState === "complete" ||
+  (document.readyState !== "loading" && !document.documentElement.doScroll)
 ) {
-    application.windowLoad();
+  application.windowLoad();
 } else {
-    document.addEventListener("DOMContentLoaded", application.windowLoad);
+  document.addEventListener("DOMContentLoaded", application.windowLoad);
 }
 
-if ('serviceWorker' in navigator) {
-    window.addEventListener('load', () => {
-        navigator.serviceWorker
-            .register('../sw.js')
-            .then(registration = {})
-            .catch(err => console.error(`Service Worker: Error: ${err}`));
-    });
+/**
+ * Check is servicerworkers are supported & register if true
+ */
+if ("serviceWorker" in navigator) {
+  window.addEventListener("load", () => {
+    navigator.serviceWorker
+      .register("../sw.js")
+      .catch((err) => console.error(`Service Worker: Error: ${err}`));
+  });
 }
