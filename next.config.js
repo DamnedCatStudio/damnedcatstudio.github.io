1bb0a3371d87c3da003ed36760fdad2ee65b0c5e
John Weland <john.weland@gmail.com>
2022-04-17 17:57:43 -0500

    prep for pr

--- next.config.js
+++ next.config.js
@@ -2,9 +2,11 @@
 const nextConfig = {
   reactStrictMode: true,
   images: {
-    domains: ['tailwindui.com', 'images.unsplash.com'],
+    domains: ['tailwindui.com', 'images.unsplash.com', 'damnedcat-studio.imgix.net'],
     dangerouslyAllowSVG: true,
     contentSecurityPolicy: "default-src 'self'; script-src 'none'; sandbox;",
+    loader: 'imgix',
+    path: 'https://damnedcat.studio/images',
   }
 }
 
