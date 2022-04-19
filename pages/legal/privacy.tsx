7960e70768ad58f4b79a763404f329939cb24d84
John Weland <john.weland@gmail.com>
2022-04-18 21:39:57 -0500

    added form hook, deploying to test

--- pages/legal/privacy.tsx
+++ pages/legal/privacy.tsx
@@ -3,7 +3,7 @@ import Link from 'next/link';
 const PrivacyPolicy = () => {
     return (
         <>
-            <article className="prose dark:prose-dark">
+            <article className="prose dark:prose-dark max-w-7xl mx-auto py-12 px-4 sm:px-6 lg:px-8 lg:py-24">
                 <h1>Privacy Policy</h1>
                 <p>Last updated: April 16, 2022</p>
                 <p>This Privacy Policy describes Our policies and procedures on the collection, use and disclosure of Your information when You use the Service and tells You about Your privacy rights and how the law protects You.</p>
